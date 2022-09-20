d0807d84fb2f21fac5a04feb38ed2fb487b54121
Fabio <fabio.lovetro@gmail.com>
2022-09-20 18:14:38 +0100

    added methods addToX(int value) and addToY(int value)

---
+++ Mars-Rover-Kata/ICoordinates.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mars_Rover_Kata
+{
+    /// <summary>
+    /// Interface Coordinates
+    /// </summary>
+    public interface ICoordinates
+    {
+        /// <summary>
+        /// Compare two coordinates. Return true if the coordinates are equal
+        /// </summary>
+        /// <param name="coordinate">The coordinate to compare</param>
+        public bool compareCoordinates(ICoordinates coordinate);
+        /// <summary>
+        /// Add the value to the x axe of the coordinate
+        /// </summary>
+        public int addToX(int value);
+        /// <summary>
+        /// Add the value to the y axe of the coordinate
+        /// </summary>
+        public int addToY(int value);
+    }
+}
