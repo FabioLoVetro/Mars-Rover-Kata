53aaed8799ab99d70b4c4b56104e3b1611eccdb0
Fabio <fabio.lovetro@gmail.com>
2022-09-21 22:07:01 +0100

    Position implementation

--- Mars-Rover-Kata/Position.cs
+++ Mars-Rover-Kata/Position.cs
@@ -10,42 +10,58 @@ namespace Mars_Rover_Kata
     /// </summary>
     public class Position
     {
+        private ICoordinates coordinate;
+        private string orientation;
+
         /// <param name="coordinates">The coordinates of the position</param>
         /// <param name="orientation">The orientation of the position</param>
         public Position(ICoordinates coordinates, string orientation)
         {
-            throw new System.NotImplementedException();
+            this.coordinate = coordinates;
+            this.orientation = orientation;
         }
 
         /// <summary>
         /// The coordinates of the position
         /// </summary>
-        public ICoordinates coordinate
+        public ICoordinates Coordinate
         {
-            get => default;
-            set
-            {
-            }
+            get => this.coordinate;
+            set => this.coordinate = value;
         }
 
         /// <summary>
         /// The orientation of the position
         /// </summary>
-        public string orientation
+        public string Orientation
         {
-            get => default;
-            set
-            {
-            }
+            get => this.orientation;
+            set => this.orientation = value;
         }
 
         /// <summary>
         /// Compute a new position after istruction of movement
         /// </summary>
         /// <param name="instruction">The instruction of movement</param>
-        public void computeNewPosition(string instruction)
+        public void ComputeNewPosition(string instruction)
         {
-            throw new System.NotImplementedException();
+            string newOrientation = this.Orientation;
+            foreach (char s in instruction)
+            {
+                if (this.orientation == "N" && s == 'L') newOrientation = "O";
+                if (this.orientation == "N" && s == 'R') newOrientation = "E";
+                if (this.orientation == "E" && s == 'L') newOrientation = "N";
+                if (this.orientation == "E" && s == 'R') newOrientation = "S";
+                if (this.orientation == "S" && s == 'L') newOrientation = "E";
+                if (this.orientation == "S" && s == 'R') newOrientation = "O";
+                if (this.orientation == "O" && s == 'L') newOrientation = "S";
+                if (this.orientation == "O" && s == 'R') newOrientation = "N";
+                this.orientation = newOrientation;
+                if (this.orientation == "N" && s == 'M') this.coordinate.AddToY(+1);
+                if (this.orientation == "E" && s == 'M') this.coordinate.AddToX(+1);
+                if (this.orientation == "S" && s == 'M') this.coordinate.AddToY(-1);
+                if (this.orientation == "O" && s == 'M') this.coordinate.AddToX(-1);
+            }
         }
     }
 }
