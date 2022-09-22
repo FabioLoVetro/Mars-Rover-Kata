5d5314239f4335c5a52b7592b32e317c3ba241ba
Fabio <fabio.lovetro@gmail.com>
2022-09-22 08:46:58 +0100

    Tools class first structure

---
+++ Mars-Rover-Kata/Tools.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mars_Rover_Kata
+{
+    /// <summary>
+    /// The class rappresent a tool for the rover
+    /// </summary>
+    public abstract class Tools
+    {
+        private string name;
+        private int weight;
+
+        /// <summary>
+        /// The name of the tool
+        /// </summary>
+        private string Name
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// The weight of the tool
+        /// </summary>
+        public int Weight
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
