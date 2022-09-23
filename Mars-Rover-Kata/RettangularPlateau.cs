f1b0ca32938ac931424f4dcbdb1b67322053b408
Fabio <fabio.lovetro@gmail.com>
2022-09-23 16:56:06 +0100

    Rettangular plateau implementation

---
+++ Mars-Rover-Kata/RettangularPlateau.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mars_Rover_Kata
+{
+    /// <summary>
+    /// A Rectangular Plateau is a rectangular shaped Plateau
+    /// </summary>
+    public class RettangularPlateau : Plateau
+    {
+        private CartesianCoordinates _size;
+
+        /// <param name="size">The size of the Plateau</param>
+        public RettangularPlateau(CartesianCoordinates size) : base()
+        {
+            this._size = size;
+        }
+
+        /// <summary>
+        /// The size of the Plateau
+        /// </summary>
+        public CartesianCoordinates Size
+        {
+            get { return this._size; }
+            set { this._size = value; }
+        }
+    }
+}
