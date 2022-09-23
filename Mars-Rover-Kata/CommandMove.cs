e990f4b1988087662796f985205c6eb92f3cdfde
Fabio <fabio.lovetro@gmail.com>
2022-09-23 12:02:10 +0100

    CommandMove first structure

---
+++ Mars-Rover-Kata/CommandMove.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mars_Rover_Kata.Interface;
+
+namespace Mars_Rover_Kata
+{
+    /// <summary>
+    /// Command Move
+    /// </summary>
+    public class CommandMove : ICommand
+    {
+        public bool commandValidator(string instruction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void execute(string instruction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
