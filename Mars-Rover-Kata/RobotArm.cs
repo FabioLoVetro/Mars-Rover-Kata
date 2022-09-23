afee81639ce8984f1fc4dd9dd85920ab0a49b62e
Fabio <fabio.lovetro@gmail.com>
2022-09-23 12:06:04 +0100

    RobotArm first structure

---
+++ Mars-Rover-Kata/RobotArm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mars_Rover_Kata
+{
+    /// <summary>
+    /// A robot arm allow to pick up or leave object from the rover
+    /// </summary>
+    public class RobotArm : Tools
+    {
+        private string name;
+        private int weight;
+
+        /// <param name="name">The name</param>
+        /// <param name="weight">The weight</param>
+        public RobotArm(string name, int weight): base (name, weight)
+        {
+        }
+    }
+}
