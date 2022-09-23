7f0ff74f6832a170b5bbe896e0b8997445bcb5d2
Fabio <fabio.lovetro@gmail.com>
2022-09-23 12:01:31 +0100

    Camera first structure

---
+++ Mars-Rover-Kata/Camera.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mars_Rover_Kata
+{
+    /// <summary>
+    /// A special camera allow to make photo with different filters.
+    /// </summary>
+    public class Camera : Tools
+    {
+        /// <param name="name">The name</param>
+        /// <param name="weight">The weight</param>
+        public Camera(string name, int weight): base (name, weight)
+        {
+        }
+
+        /// <summary>
+        /// A filter for the camera
+        /// </summary>
+        public string filter
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
