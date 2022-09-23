133d3b2743af2317ddfb89235ffd6beb51670b64
Fabio <fabio.lovetro@gmail.com>
2022-09-23 12:02:51 +0100

    ICommand first structure

---
+++ Mars-Rover-Kata/Interface/ICommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mars_Rover_Kata.Interface
+{
+    /// <summary>
+    /// Interface Command
+    /// </summary>
+    public interface ICommand
+    {
+        /// <summary>
+        /// Execute the command
+        /// </summary>
+        /// <param name="instruction">The instruction to execute</param>
+        public void execute(string instruction);
+        /// <summary>
+        /// Validate the instruction to execute
+        /// </summary>
+        public bool commandValidator(string instruction);
+    }
+}
