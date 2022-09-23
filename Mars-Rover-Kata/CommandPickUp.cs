c154f4ce9bb96d1219cbb91640e58ca949608176
Fabio <fabio.lovetro@gmail.com>
2022-09-23 12:02:29 +0100

    CommandPickUp first structure

---
+++ Mars-Rover-Kata/CommandPickUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mars_Rover_Kata
+{
+    /// <summary>
+    /// Command Pick Up
+    /// </summary>
+    public class CommandPickUp : ICommand
+    {
+        public bool commandValidator(string instruction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void execute(string instruction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
