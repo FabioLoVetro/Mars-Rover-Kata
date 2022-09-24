5de8523ef8a689ca399793133305d1df996b87b6
Fabio <fabio.lovetro@gmail.com>
2022-09-24 18:42:56 +0100

    new disposition in package

---
+++ Mars-Rover-Kata/Command/PickUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mars_Rover_Kata.Logic;
+using Mars_Rover_Kata.Model.Interface;
+using Mars_Rover_Kata.Model.Abstract;
+
+namespace Mars_Rover_Kata.Command
+{
+    /// <summary>
+    /// Command Pick Up
+    /// </summary>
+    public class PickUp : ICommand
+    {
+
+        public void Execute(MissionControl mc, Plateau p, string[] instruction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
